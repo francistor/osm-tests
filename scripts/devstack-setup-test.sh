cd8f26ac216071417775f1498126caa25a2d671a
francistor <francisco.cardosogil@gmail.com>
2020-10-29 21:57:13 +0100

    Use project in security group creation

--- scripts/devstack-setup-test.sh
+++ scripts/devstack-setup-test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Update the quota for the number of instances
-openstack quota set --instances 100 $(openstack project show -f value -c id test-project)
+test_project=$(openstack project show -f value -c id test-project)
+openstack quota set --instances 100 $test_project
 # Update the quota for the number of CPU
-openstack quota set --cores 100 $(openstack project show -f value -c id test-project)
+openstack quota set --cores 100 $test_project
 
-# Create my_security_group
-openstack security group create my_security_group
+# Create an specific security group with custom rules
+openstack security group create my_security_group --project $test_project
 sec_group=$(openstack security group list|grep my_security_group|cut -d " " -f 2)
 openstack security group rule create --protocol icmp --remote-ip 0.0.0.0/0 $sec_group
 openstack security group rule create --protocol tcp --remote-ip 0.0.0.0/0 --dst-port 22 $sec_group
