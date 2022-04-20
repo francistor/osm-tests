7edaa3f26eeb316b4fffa72c3806306ea08dfe90
francistor <francisco.cardosogil@gmail.com>
2022-04-20 22:12:32 +0200

    message

--- mongocleaner/osm_mongo_cleaner.sh
+++ mongocleaner/osm_mongo_cleaner.sh
@@ -8,15 +8,16 @@ then
     echo "  Standard usage consists on executing with the mark-orphans option, which will rename the orphan files to include";
     echo "  the string ORPHAN_ as a prefix, and then with the delete-orphans option, which will remove those files";
     echo "  This is done for safety: the user may check that the initial step does not do any harm, or revert with revert-orphans";
-    echo "  if necessary.
-    echo ""
-    echo "  orphan-folders: shows the orphan folders. No changes are performed";
-    echo "  orphan-files: shows all the orphan files. No changes are performed";
-    echo "  mark-orphans: prepeds ORPHAN_ to any orphan files";
-    echo "  delete-orphans: deletes the files marked as orphan in the previous step;
-    echo "  revert-orphans: removes the ORPHAN_ prefix in any files";
-    echo "  all-files: list all files, orphan or not";
-    echo "  all-folders: list all folders, orphan or not";
+    echo "  if necessary.";
+    echo "";
+    echo "  COMMANDS";
+    echo "    orphan-folders: shows the orphan folders. No changes are performed";
+    echo "    orphan-files: shows all the orphan files. No changes are performed";
+    echo "    mark-orphans: prepeds ORPHAN_ to any orphan files";
+    echo "    delete-orphans: deletes the files marked as orphan in the previous step";
+    echo "    revert-orphans: removes the ORPHAN_ prefix in any files";
+    echo "    all-files: list all files, orphan or not";
+    echo "    all-folders: list all folders, orphan or not";
     exit 0;
 fi
 
@@ -29,7 +30,7 @@ fi
 # Do the job
 # Use brackets to concat the stdout. First, define a variable with the command to execute.
 # Use this if executing from outside mongo but having access to kubectl
-# { echo "var action=\""$1"\""; cat osm_mongo_cleaner.js; } | kubectl exec -i -c mongodb-k8s -n osm mongodb-k8s-0 -- mongo --quiet
+{ echo "var action=\""$1"\""; cat osm_mongo_cleaner.js; } | kubectl exec -i -c mongodb-k8s -n osm mongodb-k8s-0 -- mongo --quiet
 
 # With access to mongo
-{ echo "var action=\""$1"\""; cat osm_mongo_cleaner.js; } | mongo --quiet
+# { echo "var action=\""$1"\""; cat osm_mongo_cleaner.js; } | mongo --quiet
